a06e5448d3ba8f4ffac8c9a6f8003557d1f02a07
usuario <usuario@10.22.143.13>
2015-10-27 09:25:11 +0000

    kata 4 primera versión

---
+++ src/kata4/Kata4.java
@@ -0,0 +1,14 @@
+package kata4;
+
+import java.io.File;
+
+public class Kata4 {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\usuario\\Desktop");
+        String[] names = file.list();
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
